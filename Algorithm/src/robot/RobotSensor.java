67152f1443c286017b15431f5e1e856ff1d7e67c
CharlotteChng <chng.charlotte@gmail.com>
2020-09-06 22:28:09 +0800

    version 2

--- Algorithm/src/robot/RobotSensor.java
+++ Algorithm/src/robot/RobotSensor.java
@@ -41,20 +41,6 @@ public class RobotSensor {
 		return distance;
 	}
 
-	public int[] getSensorDistance(OrientationType orientation, RangeType range)) {
-		//get robot orientation 
-		
-		//get sensor data
-		sensorDistance[0] = getSensorDistance(FRONT, SHORT);
-		sensorDistance[1] = getSensorDistance(FRONT, SHORT);
-		sensorDistance[2] = getSensorDistance(FRONT, SHORT);
-		sensorDistance[3] = getSensorDistance(LEFT, SHORT);
-		sensorDistance[4] = getSensorDistance(LEFT, LONG);
-		sensorDistance[5] = getSensorDistance(RIGHT, SHORT);
-		
-		return sensorDistance;
-	}
-
 	public void setLocation(SensorDir location) {
 		this.location = location;
 	}
